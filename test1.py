68974619ef4b903f60c3d484f0bdcccfe5fd72ba
zhangx <1226965714@qq.com>
2018-04-03 10:30:33 +0800

    方程口诀

---
+++ test1.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+class Fa(object):
+    def gen(self, line):
+        for i in range(1,line+1):
+            for j in range(1,i+1):
+                print('%d*%d=%d  ' % (j,i,j*i),end="")
+            print(end='\n')
+
+if __name__ == '__main__':
+    fa = Fa()
+    fa.gen(9)
