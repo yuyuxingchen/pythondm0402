0b0f96e6b18709254a5bdcf8c5f1792b7d38400b
zhangx <1226965714@qq.com>
2018-04-07 21:23:43 +0800

    2

--- getjpg.py
+++ getjpg.py
@@ -1,9 +1,44 @@
-# -*- coding: UTF-8 -*-
-from urllib import request
-import chardet
+# # -*- coding: UTF-8 -*-
+# from urllib import request
+# import re
+# def getHtml(url):
+#     page = request.urlopen(url)
+#     html = page.read().decode(encoding='utf-8',errors='strict')
+#     return html
+#
+# def getjpg(html):
+#      rs = r'src=(http.*?\.jpg)'
+#      imgre = re.compile(rs)
+#      jpg = re.findall(imgre,str(html))
+#     # return jpg
+#      x = 0
+#      for imgurl in jpg:
+#         request.urlretrieve(imgurl,'%s.jpg' % x)
+#         x+=1
+#
+# # html = getHtml("https://image.baidu.com/")
+# # lines = getjpg(html)
+# # file = open('a','w')
+# # file.write(str(lines))
+# print(getHtml("https://image.baidu.com/"))
 
-if __name__ == "__main__":
-    response = request.urlopen("http://fanyi.baidu.com/")
-    html = response.read()
-    charset = chardet.detect(html)
-    print(charset)
+# -*- coding: utf-8 -*-
+import urllib.request
+import re
+url='https://tieba.baidu.com/index.html?traceid='
+
+def getHtml(url):
+    page = urllib.request.urlopen(url)
+    html = page.read().decode(encoding='UTF-8',errors='strict')
+    return html
+def getjpg(html):
+      rs = r'src="http.*?\.jpg"'
+      imgre = re.compile(rs)
+      jpg = re.findall(imgre,str(html))
+      return jpg
+
+h = getjpg(getHtml(url))
+file = open('fa.py', 'w')
+file.write(str(h))
+#     return html
+# print(getHtml(url))
