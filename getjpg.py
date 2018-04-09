9f9f889c1f981945d10c2469ec935d55f0ab27c7
zhangx <1226965714@qq.com>
2018-04-09 10:11:43 +0800

    2

--- getjpg.py
+++ getjpg.py
@@ -2,32 +2,47 @@
 import urllib.request
 import re
 import os
+
+
 url='https://tieba.baidu.com/index.html?traceid'
+dirname = 'picture'
+res = r'src="(https://[A-Z0-9a-z\s\%\.\_\/-=]*?\.jpg)"'
 
 def getHtml(url):
     page = urllib.request.urlopen(url)
     html = page.read().decode(encoding='UTF-8',errors='strict')
     return html
 
-def getjpgurl(html):
-    rs = r'src="(https://[A-Z0-9a-z\s\%\.\_\/-=]*?\.jpg)"'
+
+def getJpgurl(html):
+    rs = res
     imgre = re.compile(rs,re.S)
     jpgurl = re.findall(imgre,html)
     return jpgurl
 
-def getjpg(jpgurl):
-    jpglist = []
-    os.chdir('picture')
-    x = 0
-    for i in range(10):
-        jpglist.append(jpgurl[i])
-    for jpgurl in jpglist:
-        urllib.request.urlretrieve(jpgurl, '%s.jpg' % x)
-        x = x + 1
 
-html = getHtml(url)
-getjpgurl = getjpgurl(html)
-getjpg(getjpgurl)
+def getJpg(jpgurl):
+    jpglist = []
+    if os.path.exists(dirname):
+        os.chdir(dirname)
+    else:
+        os.mkdir(dirname)
+        os.chdir(dirname)
+        x = 0
+        for i in range(20):
+            jpglist.append(jpgurl[i])
+        for jpgurl in jpglist:
+            if os.path.isfile('%s.jpg'):
+                pass
+            else:
+                urllib.request.urlretrieve(jpgurl, '%s.jpg' % x)
+            x = x + 1
+
+
+if __name__ == '__main__':
+    html = getHtml(url)
+    getjpgurl = getJpgurl(html)
+    getJpg(getjpgurl)
 
 
 
